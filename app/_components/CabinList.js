20f69ba34e122c1bc77b27067855722e0dd7ea2c
rawera_daniel <rawera.daniel@gmail.com>
2025-01-13 19:48:36 +0300

    feat: revalidate cabin & about route to ensure up-to-date fresh data

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,7 +1,10 @@
+import { unstable_noStore as noStore } from "next/cache";
 import { getCabins } from "../_lib/data-service";
 import CabinCard from "./CabinCard";
 
 export default async function CabinList() {
+  // noStore();
+
   const cabins = await getCabins();
 
   if (!cabins) return null;
