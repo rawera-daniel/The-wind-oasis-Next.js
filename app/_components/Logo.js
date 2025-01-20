dffe726d7f862fee9679bdec0eb7e8f2e3c727fa
rawera_daniel <rawera.daniel@gmail.com>
2025-01-20 18:43:35 +0300

    refactor: move profile form into its own component as a client component

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 function Logo() {
-  console.log("Logo");
-
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
