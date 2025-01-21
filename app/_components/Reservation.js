724fd0bc7fae0f1e96f1ba36b5cfe5c2f8582cb8
rawera_daniel <rawera.daniel@gmail.com>
2025-01-21 12:27:25 +0300

    feat: implement reservation feature for cabins

---
+++ app/_components/Reservation.js
@@ -0,0 +1,17 @@
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+export default async function Reservation({ cabin }) {
+  const [setting, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+      <DateSelector setting={setting} bookedDates={bookedDates} cabin={cabin} />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
